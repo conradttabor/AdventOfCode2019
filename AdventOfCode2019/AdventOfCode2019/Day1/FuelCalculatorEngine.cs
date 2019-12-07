ae428d87bcc4f04f3da65d98ca010abf91bd0521
conradttabor <conradttabor@gmail.com>
2019-12-07 12:17:43 -0600

    Day one and day six solved

---
+++ AdventOfCode2019/AdventOfCode2019/Day1/FuelCalculatorEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2019.Day1
+{
+    public class FuelCalculatorEngine
+    {
+        public static void CalculateTotalFuelRequirement(List<int> moduleMasses)
+        {
+            var requirement = moduleMasses.Select(x => CalculateRequiredFuelMass(x)).Sum();
+            Console.WriteLine($"Fuel Requirement: {requirement}");
+            var requirementIncludingFuel = moduleMasses.Select(x => CalculateRequiredFuelMassIncludingFuelMass(x)).Sum();
+            Console.WriteLine($"Fuel Requirement Including Fuel: {requirementIncludingFuel}");
+        }
+        public static int CalculateRequiredFuelMass(double moduleMass)
+        {
+            return (int)Math.Floor(moduleMass / 3) - 2;
+        }
+
+        public static int CalculateRequiredFuelMassIncludingFuelMass(double moduleMass)
+        {
+            var initialFuelRequired = CalculateRequiredFuelMass(moduleMass);
+            var fuelNeededForFuel = CalculateFuelNeededForFuel(initialFuelRequired);
+            return initialFuelRequired + fuelNeededForFuel;
+        }
+
+        public static int CalculateFuelNeededForFuel(double initialFuel)
+        {
+            var currentRequirement = CalculateRequiredFuelMass(initialFuel);
+            var totalRequirement = currentRequirement;
+
+            while (currentRequirement > 0)
+            {
+                currentRequirement = CalculateRequiredFuelMass(currentRequirement);
+                if (currentRequirement > 0)
+                    totalRequirement += currentRequirement;
+            }
+
+            return totalRequirement;
+        }
+    }
+}
---
+++ AdventOfCode2019/AdventOfCode2019/Program.cs
@@ -0,0 +1,51 @@
+﻿using AdventOfCode2019.Day1;
+using AdventOfCode2019.Day6;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2019
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------DAY1----------");
+            var day1Inputs = GetDay1Inputs("../../../Day1/Day1Input.txt");
+            FuelCalculatorEngine.CalculateTotalFuelRequirement(day1Inputs);
+            Console.WriteLine("----------DAY6----------");
+            var day6Inputs = GetDay6Inputs("../../../Day6/Day6Input.txt");
+            var mapEngine = new UniversalOrbitalMapEngine(day6Inputs);
+            mapEngine.MapOrbits();
+        }
+
+        public static List<int> GetDay1Inputs(string filePath)
+        {
+            var inputs = new List<int>();
+            Stream stream = new FileStream(filePath, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                inputs.Add(int.Parse(line));
+            }
+            return inputs;
+        }
+
+        public static List<Tuple<string, string>> GetDay6Inputs(string filePath)
+        {
+            var inputs = new List<Tuple<string, string>>();
+            Stream stream = new FileStream(filePath, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var split = line.Split(')');
+                inputs.Add(new Tuple<string, string>(split[0], split[1].TrimEnd(',')));
+            }
+            return inputs;
+        }
+    }
+}
